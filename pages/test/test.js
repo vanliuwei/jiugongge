412d27630dadb9dd39e802bfe07866b73cdc052e
vanliuwei <liuwei@vdongchina.com>
2020-08-26 11:03:54 +0800

    九宫格

---
+++ pages/test/test.js
@@ -0,0 +1,29 @@
+Page({
+    data: {
+        dates: '2016-11-08',
+        times: '12:00',
+        objectArray: ['中国', '英国', '美国'],
+        index: 0,
+    },
+    //  点击时间组件确定事件  
+    bindTimeChange: function (e) {
+        console.log("谁哦按")
+        this.setData({
+            times: e.detail.value
+        })
+    },
+    //  点击日期组件确定事件  
+    bindDateChange: function (e) {
+        console.log(e.detail.value)
+        this.setData({
+            dates: e.detail.value
+        })
+    },
+    //  点击城市组件确定事件  
+    bindPickerChange: function (e) {
+        console.log(e.detail.value)
+        this.setData({
+            index: e.detail.value
+        })
+    }
+})
